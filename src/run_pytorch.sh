57e65d9f85acf3143df684764fa79a4964ec0224
Saurabh <saurabh@madstreetden.com>
2018-09-04 17:37:08 -0500

    sr/run_pytorch.sh : Temporary change

--- src/run_pytorch.sh
+++ src/run_pytorch.sh
@@ -1,16 +1,16 @@
-mpirun -n 3 --hostfile hosts_address \
+mpirun -n 3 \
 python distributed_nn.py \
 --lr=0.1 \
 --momentum=0.9 \
 --max-steps=100000 \
 --epochs=100 \
---network=ResNet18 \
---dataset=Cifar10 \
---batch-size=1024 \
+--network=LeNet \
+--dataset=MNIST \
+--batch-size=32 \
 --comm-type=Bcast \
 --num-aggregate=5 \
 --mode=normal \
 --eval-freq=20 \
 --compress-grad=compress \
 --enable-gpu= \
---train-dir=/home/ubuntu/MPI_shared/
+--train-dir=/home/ubuntu/shared/
